986708db5302767a05d6ba1fc4dd549a63c4d78e
Tan To <tantt3746@gmail.com>
2017-05-15 23:19:15 +0700

    AS-0001 initial project

---
+++ source code/ASearch/src/main/java/com/t3t/lol/ASearchApplication.java
@@ -0,0 +1,12 @@
+package com.t3t.lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ASearchApplication.class, args);
+	}
+}
